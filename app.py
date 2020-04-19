3b2c2110d5a471c80e5b814fa1a6b9bb37c5e044
Rachit Bansal <rachitbansal2500@hmail.com>
2020-04-20 03:02:40 +0530

    update app

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import requests
 import os
 # import pickle as pkl
 import praw
-import nltk
-from nltk.stem import WordNetLemmatizer
+# import nltk
+# from nltk.stem import WordNetLemmatizer
 # from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 # from sklearn.pipeline import make_pipeline
-from nltk.corpus import stopwords
+# from nltk.corpus import stopwords
 import re
 # import contractions
 # import sklearn
@@ -31,9 +31,9 @@ def main():
         
         sub = reddit.submission(url=url)
         data = [sub.title, sub.url, sub.selftext, sub.link_flair_text]
-        data[0] = process(data[0])
-        data[1] = processURL(data[1])
-        data[2] = process(data[2])
+        # data[0] = process(data[0])
+        # data[1] = processURL(data[1])
+        # data[2] = process(data[2])
 
         # model = pkl.load(open('model_lr.pkl', 'rb'))
         # prediction = model.predict([data[0] + ' ' + data[2] + ' ' + data[1]])
@@ -41,29 +41,29 @@ def main():
 
         return flask.render_template('index.html', pred = actual, act = actual)
 
-wnl = WordNetLemmatizer()
-remove =set(stopwords.words('english'))
+# wnl = WordNetLemmatizer()
+# remove =set(stopwords.words('english'))
 
-def process(words):
-    words = str(words)
-    words = re.sub('([.,////])',' ',words)
-    words = words.replace('\n', ' ')
-    try:
-      words = contractions.fix(words)
-    except:
-      pass
-    word_list = nltk.word_tokenize(re.sub(r'([^a-z A-Z])', '', words.lower()))
-    comment = ' '.join([wnl.lemmatize(w) for w in word_list if w not in remove])
+# def process(words):
+#     words = str(words)
+#     words = re.sub('([.,////])',' ',words)
+#     words = words.replace('\n', ' ')
+#     try:
+#       words = contractions.fix(words)
+#     except:
+#       pass
+#     word_list = nltk.word_tokenize(re.sub(r'([^a-z A-Z])', '', words.lower()))
+#     comment = ' '.join([wnl.lemmatize(w) for w in word_list if w not in remove])
     
-    return comment
+#     return comment
 
-def processURL(words):
-    try:
-      words = words.split('://')[1]
-      words = words.split('/')
-      return ' '.join([' '.join(word.split('.')) for word in words])
-    except:
-      return words
+# def processURL(words):
+#     try:
+#       words = words.split('://')[1]
+#       words = words.split('/')
+#       return ' '.join([' '.join(word.split('.')) for word in words])
+#     except:
+#       return words
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
